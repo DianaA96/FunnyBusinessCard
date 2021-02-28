67d0ae79d5f13a4f37b8c2f72af34daebea2a23d
Diana Aguirre <>
2021-02-28 16:14:13 -0600

    El componente en App.js ahora renderea al componente Card.Dentro de App.js se declara un objeto que luego se pasa como props al componente Card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Card from './Card'
+
+let diana = {nombres: "Diana Guadalupe", apellidos: "García Aguirre", logosLinks: ["diana.aguirre.0011", "DianaA96", "dianaa0011", "diana-garcia-aguirre", "dianaa960611@gmail.com"] };
 
 function App() {
   return (
-    <h1 id='Prueba'></h1>
+    <Card {...diana}/>
   );
 }
 
