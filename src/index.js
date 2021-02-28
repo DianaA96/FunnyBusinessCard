84763a317717a132eade41aa35706a2b41768bf1
Diana Aguirre <>
2021-02-28 16:10:24 -0600

    index.js ya no contiene código JSX que renderee directamente el componente Card

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Card from './Card';
 import Bubbles from './Bubbles';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Bubbles />
     <App />
-    <Card />
   </React.StrictMode>,
   document.getElementById('root')
 );
